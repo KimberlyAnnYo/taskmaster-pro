e7fdd557c255faaf355fb6ffea15f510a5f67593
Kimberly West <kdenton993@gmail.com>
2022-03-11 20:50:30 -0600

    draggable and removable

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,35 +54,35 @@ $(".list-group").on("click", "p", function () {
     .val(text)
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
-  
+
 });
 $(".list-group").on("blur", "textarea", function () {
   // get the textarea's current value/text
   var text = $(this)
-  .val()
-  .trim();
+    .val()
+    .trim();
   // get the parent ul's id attribute
   var status = $(this)
-  .closest(".list-group")
-  .attr("id")
-  .replace("list-", "");
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
   // get the task's position in the list of other li elements
   var index = $(this)
-  .closest(".list-group-item")
-  .index();
+    .closest(".list-group-item")
+    .index();
   tasks[status][index].text = text;
   saveTasks();
   //recreate p element
   var taskP = $("<p>")
-  .addClass("m-1")
-  .text(text);
-  
+    .addClass("m-1")
+    .text(text);
+
   // replace texterea with p element
   $(this).replaceWith(taskP);
 });
 
 // due date was clicked
-$(".list-group").on("click", "span", function() {
+$(".list-group").on("click", "span", function () {
   // get current text
   var date = $(this)
     .text()
@@ -101,22 +101,22 @@ $(".list-group").on("click", "span", function() {
   dateInput.trigger("focus");
 });
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function(){
+$(".list-group").on("blur", "input[type='text']", function () {
   // get current text
   var date = $(this)
-  .val()
-  .trim();
+    .val()
+    .trim();
 
   // get the parent ul's id attribute
   var status = $("this")
-  .closest(".list-group")
-  .attr("id")
-  .replace("list-", "");
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
 
   // get the task's position in the list of other li elements
   var index = $(this)
-  .closest(".list-group-item")
-  .index();
+    .closest(".list-group-item")
+    .index();
 
   // update task in arra and resave to local storage
   task[status][index].date = date;
@@ -124,8 +124,8 @@ $(".list-group").on("blur", "input[type='text']", function(){
 
   // recreate span element with bootstrap classes
   var taskSpan = $("<span>")
-  .addClass("badge badge-primart badge-pill")
-  .text(date);
+    .addClass("badge badge-primart badge-pill")
+    .text(date);
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
@@ -173,5 +173,71 @@ $("#remove-tasks").on("click", function () {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+  connectWith: (".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("deavtivate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    // array to store the task data in
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    console.log(tempArr);
+
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks()
+  }
+});
+
+//drag and drop trash
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  }, 
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 // load tasks for the first time
 loadTasks();
